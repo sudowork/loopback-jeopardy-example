60ea679a77e5a483b5003e715a859dad7d1ed98b
Kevin Gao <kevin.gao@datafox.co>
2017-09-12 15:52:47 -0500

    Add GET /jeopardyQuestions/random

--- common/models/jeopardy-question.js
+++ common/models/jeopardy-question.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = function(Jeopardyquestion) {
-
+  Jeopardyquestion.remoteMethod(
+    'random',
+    {
+      http: {
+        path: '/random',
+        verb: 'get',
+      },
+      description: 'Gets one random question',
+      returns: {
+        arg: 'questions',
+        type: 'json',
+      },
+    }
+  );
+  Jeopardyquestion.random = (callback) => {
+    Jeopardyquestion.getDataSource().connector.connect((err, db) => {
+      const collection = db.collection('jeopardy');
+      collection.aggregate([
+        {$sample: {size: 1}},
+      ], (err, data) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, data);
+      });
+    });
+  };
 };
